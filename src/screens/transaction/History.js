45b5c30ac4fbeb6dc4ee80fb4c08560f0f8ee457
pridantaraka <pridantaraka@gmail.com>
2022-10-17 20:17:13 +0700

    see all transaction History

--- src/screens/transaction/History.js
+++ src/screens/transaction/History.js
@@ -8,31 +8,44 @@ import {
 } from 'react-native';
 import styles from '../../styles/global';
 import React from 'react';
-import Card from '../../components/Card';
+import {CardHistory} from '../../components/Card';
 import Data from '../../assets/Data';
+import {historyTransaction} from '../../redux/asyncActions/transaction';
+import {useSelector, useDispatch} from 'react-redux';
 
 const History = ({navigation}) => {
+  const dispatch = useDispatch();
+  const dataHistory = useSelector(state => state.transaction.data);
+  const token = useSelector(state => state.auth.token);
+  const [limit, setLimit] = React.useState(5);
+  const [sortType, setSortType] = React.useState('ASC');
+  React.useEffect(() => {
+    dispatch(historyTransaction({token, limit, sortType}));
+  }, [token, limit, sortType, dispatch]);
   return (
-    <ScrollView style={styles.wrapper}>
-      <View style={styles.content}>
-        <View style={styles.wrapHistory}>
-          <View>
-            <Text style={styles.font1}>Transaction History</Text>
+    <>
+      <ScrollView style={styles.wrapper}>
+        <View style={styles.content}>
+          <View style={styles.wrapHistory}>
+            <View>
+              <Text style={styles.font1}>Transaction History</Text>
+            </View>
           </View>
+          <FlatList
+            data={dataHistory}
+            renderItem={({item}) => {
+              return (
+                <TouchableOpacity
+                  onPress={() => navigation.navigate('Transfer')}>
+                  <CardHistory item={item} />
+                </TouchableOpacity>
+              );
+            }}
+            keyExtractor={item => String(item.id_transaction)}
+          />
         </View>
-        <FlatList
-          data={Data}
-          renderItem={({item}) => {
-            return (
-              <TouchableOpacity onPress={() => navigation.navigate('Transfer')}>
-                <Card item={item} />
-              </TouchableOpacity>
-            );
-          }}
-          keyExtractor={item => String(item.id)}
-        />
-      </View>
-    </ScrollView>
+      </ScrollView>
+    </>
   );
 };
 
